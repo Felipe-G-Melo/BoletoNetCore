fab98dd5b1a90ca45446d3e06c64d7b9d919a6d9
Felipe Melo <melolipedev@gmail.com>
2024-08-09 11:27:19 -0300

    Implementando a carteira 9

---
+++ BoletoNetCore/Banco/Sicoob/Carteiras/BancoSicoobCarteira9.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BoletoNetCore
+{
+    [CarteiraCodigo("9/01")]
+    internal class BancoSicoobCarteira9: ICarteira<BancoSicoob>
+    {
+        internal static Lazy<ICarteira<BancoSicoob>> Instance { get; } = new Lazy<ICarteira<BancoSicoob>>(() => new BancoSicoobCarteira9());
+
+        private BancoSicoobCarteira9()
+        {
+
+        }
+
+        public string FormataCodigoBarraCampoLivre(Boleto boleto)
+        {
+            return "                         ";
+        }
+
+        public void FormataNossoNumero(Boleto boleto)
+        {
+        }
+    }
+}
